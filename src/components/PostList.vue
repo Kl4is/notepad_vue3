6873ca96f4e21ab5cce06a4b7dceaed410f6a282
Kl4is <dimaischencko@yandex.ru>
2023-10-06 00:05:46 +0300

    notepad

---
+++ src/components/PostList.vue
@@ -0,0 +1,53 @@
+<template>
+    <div v-if="posts.length > 0">
+        <transition-group name="post-list">
+            <post-item
+                v-for="post in posts" 
+                :post = "post"
+                :key="post.id"
+                @remove = "$emit('remove', post)"
+            />    
+        </transition-group>
+    </div> 
+    <h2 v-else style=" color: brown;">
+        Список заметок пуст
+    </h2>
+</template>
+
+<script>
+import PostItem from '@/components/PostItem'
+export default{
+    components: {PostItem},
+    props: {
+        posts: {
+            type: Array,
+            required: true,
+        }
+    }
+}
+</script>
+
+<style scoped>
+.post{
+    padding: 15px;
+    border: 2px solid red;
+    margin-top: 15px;
+    border-radius: 15px;
+}
+.post-list-item{
+    display: inline-block;
+    margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active{
+    transition: all .4s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to{
+    opacity: 0;
+    transform: translateX(130px);
+}
+.post-list-move {
+  transition: transform 0.4s ease;
+}
+</style>
---
+++ src/pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Создано в учебных целях</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
